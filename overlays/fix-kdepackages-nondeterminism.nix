9abe8eb1bf7271abcc6a4fea587e0a28517fbbc8
Nowaaru <nowaaru@proton.me>
2025-06-30 23:59:27 -0700

    feat: delete overlays

--- overlays/fix-kdepackages-nondeterminism.nix
+++
@@ -1,6 +0,0 @@
-withSystem: inputs: (final: prev:
-{
-  # TODO: make this better plz use withSystem with flakeParts easyOverlay and 
-  # consume that here plz :sob: 
-  # inherit (import inputs.nixpkgs-master {inherit (prev) system;}) kdePackages;
-})
