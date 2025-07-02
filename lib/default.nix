b0848febce1c2f2217579889fc585868fbe4a4f6
Nowaaru <nowaaru@proton.me>
2025-07-01 21:06:54 -0700

    feat: implement library bootstrapping
    i don't know how or why i did this but there's a better way probably.
    i don't care. for right now. probably. i think.

--- lib/default.nix
+++ lib/default.nix
@@ -1,25 +1,26 @@
 lib: inputs: let
   specialLibraries = ["users.nix" "meta.nix"];
-  metaFunctions = import ./meta.nix (inputs // { inherit lib; });
+  metaFunctions = import ./meta.nix (inputs // {inherit lib;});
   metaLib = lib // metaFunctions;
 in
-      (lib.attrsets.foldlAttrs (
-          acc: k: _:
-            acc
-            // (let
-              importPath = ./. + "/${k}";
-            in {
-              ${lib.strings.removeSuffix ".nix" k} =
-                if (builtins.elem k specialLibraries)
-                then
-                  metaLib.withInputs importPath (inputs
-                    // {
-                      lib = metaLib;
-                    })
-                else import importPath lib;
-            })
-        ) {} (
-          lib.attrsets.filterAttrs (
-            k: v: v != "directory" && !(builtins.elem k ["default.nix"])
-          ) (builtins.readDir ./.)
-        )) // metaFunctions
+  (lib.attrsets.foldlAttrs (
+      acc: k: _:
+        acc
+        // (let
+          importPath = ./. + "/${k}";
+        in {
+          ${lib.strings.removeSuffix ".nix" k} =
+            if (builtins.elem k specialLibraries)
+            then
+              metaLib.withInputs importPath (inputs
+                // {
+                  lib = metaLib // {gamindustri = acc;};
+                })
+            else import importPath lib;
+        })
+    ) {} (
+      lib.attrsets.filterAttrs (
+        k: v: v != "directory" && !(builtins.elem k ["default.nix"])
+      ) (builtins.readDir ./.)
+    ))
+  // metaFunctions
