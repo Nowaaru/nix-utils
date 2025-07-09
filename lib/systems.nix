4f531a400a20f9c37956c981f4845e16fd1b8649
Nowaaru <nowaaru@proton.me>
2025-07-08 17:52:48 -0700

    feat: add system metafile evaluation to lib
    better asset here than in gamindustri-systems

---
+++ lib/systems.nix
@@ -0,0 +1,42 @@
+{
+  inputs,
+  flake-parts-lib,
+} @ lib: let
+  inherit (flake-parts-lib) importApply;
+in {
+  evalSystemMetafile = metaFile: specialArgs: let
+    newSpecialArgs =
+      if specialArgs
+      then specialArgs
+      else {};
+  in
+    lib.evalModules {
+      modules = [
+        (importApply ./schema/system-meta.nix {
+          inherit (lib.gamindustri.meta) mkIfElse;
+          inherit (lib.options) mkOption mkEnableOption;
+          inherit (lib) types mkIf mkMerge mkForce;
+          inherit (inputs.flake-utils.lib) allSystems;
+          systemImportArgs = newSpecialArgs;
+        })
+
+        (importApply metaFile newSpecialArgs)
+      ];
+    };
+
+  evalUserMetaFile = metaFile: specialArgs: let
+    newSpecialArgs =
+      if specialArgs
+      then specialArgs
+      else {};
+  in (lib.evalModules {
+    modules = [
+      (importApply ./schema/user-meta.nix {
+        inherit lib inputs;
+        userImportArgs = newSpecialArgs;
+      })
+
+      (importApply metaFile newSpecialArgs)
+    ];
+  });
+}
