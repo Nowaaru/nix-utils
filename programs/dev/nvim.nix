a6af058adc34e0e932921e8bd8e059b0a95bf135
Nowaaru <nowaaru@proton.me>
2025-07-11 19:41:35 -0700

    feat: add config attribute to nvf config

--- programs/dev/nvim.nix
+++ programs/dev/nvim.nix
@@ -1,12 +1,14 @@
 {
   inputs,
   configure,
+  config,
+  pkgs,
   ...
 }: {
   imports = [inputs.nvf.homeManagerModules.default];
 
   programs.nvf = {
     enable = true;
-    settings.vim = configure "nvf";
+    settings.vim = configure "nvf" config;
   };
 }
